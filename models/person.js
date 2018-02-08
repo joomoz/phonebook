ef775ea0d90e2b78cd985202176b471c7d4f2d9d
J. Moilanen <jnsmoilanen@gmail.com>
2018-02-08 14:03:54 +0200

    Env var confs.

--- models/person.js
+++ models/person.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
-var Schema = mongoose.Schema;
+var Schema = mongoose.Schema
+
+if ( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
 
 const url = process.env.MONGODB_URI
 
